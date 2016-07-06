72f0fe88f72da144f2cbf72cfbda05dce60609c2
fire855 <thefire855@gmail.com>
2016-07-06 19:30:25 +0200

    Remove sensor calibration lib & add linking for lib compilation

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libhwm
+LOCAL_SRC_FILES_64 := lib64/libhwm.so
+LOCAL_SRC_FILES_32 := lib/libhwm.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
